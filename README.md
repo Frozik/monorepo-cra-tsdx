f8584fd0437d0fcb3f4a44081fd2373dea67b26c
Dmitry Sharov <frozik@gmail.com>
2020-11-07 22:51:40 +0300

    chore: Cleaned projects code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 https://www.youtube.com/watch?v=7FWG3tBTnFM
 https://dev.to/ganeshagrawal/how-to-setup-https-locally-with-create-react-app-e46
+
+
+npx tsdx create
--- packages/apps/frontend/src/app.tsx
+++ packages/apps/frontend/src/app.tsx
@@ -1,6 +1,5 @@
 import './app.css';
 
-import { Thing } from '@shared/components';
 import React from 'react';
 
 import logo from './logo.svg';
@@ -8,7 +7,6 @@ import logo from './logo.svg';
 export const App = React.memo(() => (
   <div className="App">
     <header className="App-header">
-      <Thing />
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
